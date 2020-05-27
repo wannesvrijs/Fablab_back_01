e18b0560dcbc36692b8dd1e7df36dc11d15bf143
wannes <wannes.vrijs@gmail.com>
2020-05-27 18:55:35 +0200

    uplifting filter on machinestaat to machinecategorie

--- src/Entity/Machine.php
+++ src/Entity/Machine.php
@@ -8,10 +8,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
  * @ApiResource(
  *     itemOperations={
+ *          "get",
  *          "put"={"security"="is_granted('ROLE_ADMIN')"},
  *          "delete"={"security"="is_granted('ROLE_ADMIN')"}
  *     },
@@ -117,7 +119,7 @@ class Machine
 
     /**
      * @ORM\OneToMany(targetEntity=MachineStaat::class, mappedBy="mstaatMach")
-     * @Groups({"machinecategorie:read","machine:read","admin:write"})
+     * @Groups({"admin:write"})
      */
     private $machineStaten;
 
@@ -357,6 +359,18 @@ class Machine
     public function getMachineStaten(): Collection
     {
         return $this->machineStaten;
+    }
+
+    /**
+     * @Groups({"machinecategorie:read", "machine:read"})
+     * @SerializedName("machineStaten")
+     */
+    public function getMachineStatenUpcomming()
+    {
+        $criteria = MachineRepository::createFilterByUpcommingCriteria();
+
+        return $this->machineStaten->matching($criteria);
+
     }
 
     public function addMachineStaten(MachineStaat $machineStaten): self
--- src/Repository/MachineRepository.php
+++ src/Repository/MachineRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Machine;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -14,37 +15,19 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class MachineRepository extends ServiceEntityRepository
 {
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Machine::class);
     }
 
-    // /**
-    //  * @return Machine[] Returns an array of Machine objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public static function createFilterByUpcommingCriteria()
     {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('m.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $now = new \DateTime(null, new \DateTimeZone('Europe/Brussels'));
 
-    /*
-    public function findOneBySomeField($value): ?Machine
-    {
-        return $this->createQueryBuilder('m')
-            ->andWhere('m.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return Criteria::create()
+            ->andWhere(Criteria::expr()->gt('mstaatStop', $now))
+            ;
+
     }
-    */
 }

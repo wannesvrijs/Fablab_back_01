5b0e13fdfa6ef7eed00d52d727f268dc9cb623b4
wannes <wannes.vrijs@gmail.com>
2020-05-31 21:05:47 +0200

    new updates regarding security

--- public/index.php
+++ public/index.php
@@ -2,9 +2,26 @@
 
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
+//for local development
 require dirname(__DIR__).'/config/bootstrap.php';
+
+//for deployment system 3 
+//require 'system/config/bootstrap.php';
+
+if (!isset($_SERVER['APP_ENV'])){
+    if(!class_exists(Dotenv::class)){
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
+    }
+    //for local development
+    (new Dotenv())->load(__DIR__.'/../.env.dev.local');
+
+    //for deployment system 3
+//    (new Dotenv())->load(__DIR__.'system/.env.prod.local');
+}
+
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,9 +7,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
     /**
      * @Route("/login", name="app_login", methods={"POST"})
      */
@@ -32,5 +34,23 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('this should never be reached');
 
+    }
+
+
+    /**
+     * @Route("/adlogin", name="app_admin_login" )
+     */
+    public function adminlogin(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
     }
 }
